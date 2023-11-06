ed39fce5d39bc6d402b5e935d31122d28218c713
Erkaboy0313 <fayyozbekov20@gmail.com>
2023-11-06 12:05:00 +0500

    new build package

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import './main.scss';
+
+import i18next from "i18next";
+import {initReactI18next} from "react-i18next";
+import uz from "./locales/uz/translations.json";
+import ru from "./locales/ru/translations.json";
+import en from "./locales/en/translations.json";
+
+
+i18next.use(initReactI18next).init({
+    resources: {
+        uz: {
+            translation: uz,
+        },
+        ru: {
+            translation: ru,
+        },
+        en: {
+            translation: en,
+        },
+    },
+    lng: localStorage.getItem("lng") || "uz",
+});
+
+export default i18next;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
